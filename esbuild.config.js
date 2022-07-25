57f476d07f5a06f93137509cd2d532703349ece5
ParamagicDev <konnor5456@gmail.com>
2022-07-25 09:32:01 -0400

    esbuild final

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,35 +2,27 @@ import * as path from "path";
 import glob from "glob"
 import esbuild from "esbuild"
 
-let entries = glob
-.sync("src/**/*.{ts,js}")
-.map((file) => {
-  return path.join(path.dirname(file), path.basename(file, path.extname(file)))
-});
-
-console.log(entries)
-
-const defaultConfig = {
-  entryPoints: ["./src/index.ts"],
-  sourcemap: true,
-  platform: "browser",
-  target: "es2018",
-  watch: process.argv.includes("--watch"),
-  color: true,
-  bundle: true,
-  external: []
-}
-
-
 ;(async function () {
-  await Promise.all[
-    esbuild.build({
-      ...defaultConfig,
-      outfile: "dist/bundle/index.module.js",
-      format: "esm",
-      minify: true,
-    }),
+  let entries = glob
+    .sync("src/**/*.{ts,js}")
+    .map((file) => {
+      return path.join(path.dirname(file), path.basename(file, path.extname(file)))
+    });
 
+  const defaultConfig = {
+    entryPoints: ["./src/index.ts"],
+    sourcemap: true,
+    platform: "browser",
+    target: "es2018",
+    watch: process.argv.includes("--watch"),
+    color: true,
+    bundle: true,
+    external: []
+  }
+
+  const startTime = Number(new Date())
+
+  await Promise.all[
     esbuild.build({
       ...defaultConfig,
       outfile: "dist/bundle/index.common.js",
@@ -40,7 +32,13 @@ const defaultConfig = {
 
     esbuild.build({
       ...defaultConfig,
-      // splitting: true,
+      outfile: "dist/bundle/index.module.js",
+      format: "esm",
+      minify: true,
+    }),
+
+    esbuild.build({
+      ...defaultConfig,
       entryPoints: entries,
       outdir: 'dist',
       format: 'esm',
@@ -50,5 +48,9 @@ const defaultConfig = {
       chunkNames: 'chunks/[name]-[hash]'
     })
   ]
-  console.log("Build complete! ✨")
+
+  const endTime = Number(new Date())
+  const buildTime = endTime - startTime
+
+  console.log(`Build complete in ${buildTime}ms! ✨`)
 })()
