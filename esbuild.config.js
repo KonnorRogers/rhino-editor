a7dce80de74fbcd5a764d809796c4beb57d94b73
Konnor Rogers <konnor5456@gmail.com>
2022-07-25 09:53:31 -0400

    swap to esbuild (#1)
    * swap to esbuild
    
    * esbuild final
    
    * add types generation to deploy process

---
+++ esbuild.config.js
@@ -0,0 +1,56 @@
+import * as path from "path";
+import glob from "glob"
+import esbuild from "esbuild"
+
+;(async function () {
+  let entries = glob
+    .sync("src/**/*.{ts,js}")
+    .map((file) => {
+      return path.join(path.dirname(file), path.basename(file, path.extname(file)))
+    });
+
+  const defaultConfig = {
+    entryPoints: ["./src/index.ts"],
+    sourcemap: true,
+    platform: "browser",
+    target: "es2018",
+    watch: process.argv.includes("--watch"),
+    color: true,
+    bundle: true,
+    external: []
+  }
+
+  const startTime = Number(new Date())
+
+  await Promise.all[
+    esbuild.build({
+      ...defaultConfig,
+      outfile: "dist/bundle/index.common.js",
+      format: "cjs",
+      minify: true,
+    }),
+
+    esbuild.build({
+      ...defaultConfig,
+      outfile: "dist/bundle/index.module.js",
+      format: "esm",
+      minify: true,
+    }),
+
+    esbuild.build({
+      ...defaultConfig,
+      entryPoints: entries,
+      outdir: 'dist',
+      format: 'esm',
+      target: "es2020",
+      external: ['./node_modules/*'],
+      splitting: true,
+      chunkNames: 'chunks/[name]-[hash]'
+    })
+  ]
+
+  const endTime = Number(new Date())
+  const buildTime = endTime - startTime
+
+  console.log(`Build complete in ${buildTime}ms! ✨`)
+})()
