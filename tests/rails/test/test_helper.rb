4c99a11ce89b3d477d89c3630ee5b1be1130ff3b
Konnor Rogers <konnor5456@gmail.com>
2022-09-26 21:52:09 -0400

    add some flair

--- tests/rails/test/test_helper.rb
+++ tests/rails/test/test_helper.rb
@@ -1,6 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+require "minitest/reporters/pride_reporter"
+
+options = ENV["REPORTER"].to_s.downcase == "slow" ? {fast_fail: true, slow_count: 5} : {}
+Minitest::Reporters.use!([Minitest::Reporters::PrideReporter.new(options)])
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
