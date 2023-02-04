5a7d2ca9845d8637b7f43e5d83076b5c3dbe0a01
Konnor Rogers <konnor5456@gmail.com>
2023-02-04 14:05:23 -0500

    chore(tests): add minitest-retry

--- tests/rails/test/test_helper.rb
+++ tests/rails/test/test_helper.rb
@@ -3,7 +3,15 @@ require_relative "../config/environment"
 require "rails/test_help"
 require "minitest/reporters"
 require "minitest/reporters/pride_reporter"
+require "minitest/retry"
 
+Minitest::Retry.use!(
+  retry_count: Number(ENV.fetch("RETRY", 3)),         # The number of times to retry. The default is 3.
+  verbose: true,           # Whether or not to display the message at the time of retry. The default is true.
+  io: $stdout,             # Display destination of retry when the message. The default is stdout.
+  exceptions_to_retry: [], # List of exceptions that will trigger a retry (when empty, all exceptions will).
+  methods_to_retry:    []  # List of methods that will trigger a retry (when empty, all methods will).
+)
 options = ENV["REPORTER"].to_s.downcase == "slow" ? {fast_fail: true, slow_count: 5} : {}
 Minitest::Reporters.use!([Minitest::Reporters::PrideReporter.new(options)])
 
