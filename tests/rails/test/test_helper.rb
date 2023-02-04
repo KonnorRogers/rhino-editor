d24bf19a37a935998ebf221ff595da3747bc2814
Konnor Rogers <konnor5456@gmail.com>
2023-02-04 18:06:06 -0500

    reduce retries

--- tests/rails/test/test_helper.rb
+++ tests/rails/test/test_helper.rb
@@ -6,7 +6,7 @@ require "minitest/reporters/pride_reporter"
 require "minitest/retry"
 
 Minitest::Retry.use!(
-  retry_count: Integer(ENV.fetch("RETRY", 5)),         # The number of times to retry. The default is 3.
+  retry_count: Integer(ENV.fetch("RETRY", 2)),         # The number of times to retry. The default is 3.
   verbose: true,           # Whether or not to display the message at the time of retry. The default is true.
   io: $stdout,             # Display destination of retry when the message. The default is stdout.
   exceptions_to_retry: [], # List of exceptions that will trigger a retry (when empty, all exceptions will).
