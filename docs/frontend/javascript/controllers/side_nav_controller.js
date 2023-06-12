e3dbfce1b5c8da319557ede104cc1f767302cc02
Konnor Rogers <konnor5456@gmail.com>
2023-06-12 00:48:33 -0400

    final touches

--- docs/frontend/javascript/controllers/side_nav_controller.js
+++ docs/frontend/javascript/controllers/side_nav_controller.js
@@ -2,13 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class SideNavController extends Controller {
   async open() {
+    document.addEventListener("sl-hide", this.reset)
     this.drawer.removeAttribute("hidden")
+
+    this.scrollTop = window.scrollY;
+    document.body.classList.add('fixed-body');
+    // Scroll the wrapper, rather than setting an offset
+    // via `top` or `transform`.
+    document.body.scroll(0, this.scrollTop);
     await this.drawer.show()
   }
 
-  async close() {
+  reset = () => {
+    window.scrollTo(0, this.scrollTop);
+    document.body.classList.remove('fixed-body');
+  }
+
+  async close () {
+    document.removeEventListener("sl-hide", this.reset)
+    this.reset()
     await this.drawer.hide()
-    setTimeout(() => this.drawer.setAttribute("hidden", ""))
   }
 
   async toggle() {
