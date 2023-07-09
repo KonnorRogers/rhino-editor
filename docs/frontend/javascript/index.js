36d697b66357a40e103cdb91d4b47ad6148a1e0f
Konnor Rogers <konnor5456@gmail.com>
2023-07-09 01:34:40 -0400

    need to fix scroll locking

--- docs/frontend/javascript/index.js
+++ docs/frontend/javascript/index.js
@@ -125,32 +125,32 @@ Object.entries(controllers).forEach(([filename, controller]) => {
   }
 })
 
-if (!window.scrollPositions) {
-  window.scrollPositions = {};
-}
-
-function preserveScroll () {
-  document.querySelectorAll("[data-preserve-scroll]").forEach((element) => {
-    scrollPositions[element.id] = element.scrollTop;
-  })
-}
-
-function restoreScroll (event) {
-  document.querySelectorAll("[data-preserve-scroll]").forEach((element) => {
-    element.scrollTop = scrollPositions[element.id];
-  })
-
-  if (!event.detail.newBody) return
-  // event.detail.newBody is the body element to be swapped in.
-  // https://turbo.hotwired.dev/reference/events
-  event.detail.newBody.querySelectorAll("[data-preserve-scroll]").forEach((element) => {
-    element.scrollTop = scrollPositions[element.id];
-  })
-}
-
-window.addEventListener("turbo:before-cache", preserveScroll)
-window.addEventListener("turbo:before-render", restoreScroll)
-window.addEventListener("turbo:render", restoreScroll)
+// if (!window.scrollPositions) {
+//   window.scrollPositions = {};
+// }
+//
+// function preserveScroll () {
+//   document.querySelectorAll("[data-preserve-scroll]").forEach((element) => {
+//     scrollPositions[element.id] = element.scrollTop;
+//   })
+// }
+//
+// function restoreScroll (event) {
+//   document.querySelectorAll("[data-preserve-scroll]").forEach((element) => {
+//     element.scrollTop = scrollPositions[element.id];
+//   })
+//
+//   if (!event.detail.newBody) return
+//   // event.detail.newBody is the body element to be swapped in.
+//   // https://turbo.hotwired.dev/reference/events
+//   event.detail.newBody.querySelectorAll("[data-preserve-scroll]").forEach((element) => {
+//     element.scrollTop = scrollPositions[element.id];
+//   })
+// }
+//
+// window.addEventListener("turbo:before-cache", preserveScroll)
+// window.addEventListener("turbo:before-render", restoreScroll)
+// window.addEventListener("turbo:render", restoreScroll)
 
 function handleAttachment (event) {
   event.preventDefault()
