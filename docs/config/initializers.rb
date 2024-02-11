27d41f58e0877ce7d708b73b17989e7980f68077
Konnor Rogers <konnor5456@gmail.com>
2024-02-11 17:03:49 -0500

    update initializers

--- docs/config/initializers.rb
+++ docs/config/initializers.rb
@@ -20,3 +20,32 @@ Bridgetown.configure do |config|
   #   end
   # end
 end
+
+require "rouge"
+
+module Rouge
+  module Lexers
+    # These lexers don't implement the `prepend` class method so they'll raise an error if we don't skip them.
+    problem_lexers = [
+      ::Rouge::Lexers::ConsoleLexer,
+      ::Rouge::Lexers::Escape,
+      ::Rouge::Lexers::IRBLexer,
+      ::Rouge::Lexers::PlainText
+    ]
+
+    ::Rouge::Lexer.all.each do |lexer|
+      next if problem_lexers.include?(lexer)
+
+      # Ruleset courtesy of this issue:
+      # https://github.com/rouge-ruby/rouge/issues/642
+      lexer.prepend :root do
+        rule(/^\+.*$\n?/, lexer::Generic::Inserted)
+        rule(/^-+.*$\n?/, lexer::Generic::Deleted)
+        rule(/^!.*$\n?/, lexer::Generic::Strong)
+        rule(/^@.*$\n?/, lexer::Generic::Subheading)
+        rule(/^([Ii]ndex|diff).*$\n?/, lexer::Generic::Heading)
+        rule(/^=.*$\n?/, lexer::Generic::Heading)
+      end
+    end
+  end
+end
