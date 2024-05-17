74d3b8b4726c38dc26b4cb2ad8bf8a339c099918
Konnor Rogers <konnor5456@gmail.com>
2024-05-16 20:55:35 -0400

    fix unit tests

--- tests/unit/serializer.test.js
+++ tests/unit/serializer.test.js
@@ -16,7 +16,7 @@ test("It should properly update the input element when the serializer changes", 
   const input = div.querySelector("input")
 
   assert.equal(rhinoEditor.serializer, "html")
-  assert.equal(input.value, "")
+  assert.equal(input.value, "<p></p>")
 
   rhinoEditor.serializer = "json"
 
